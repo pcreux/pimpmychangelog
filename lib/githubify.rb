ca07a0a103733d0a22166bda2a64da9f00c8e609
Philippe Creux <pcreux@gmail.com>
2011-08-24 19:09:35 -0700

    Add #better_changelog method

--- lib/githubify.rb
+++ lib/githubify.rb
@@ -1,9 +1,14 @@
 class Githubifier
+
   attr_reader :user, :project, :changelog
 
   def initialize(user, project, changelog)
     @user = user
     @project = project
     @changelog = changelog
+  end
+
+  def better_changelog
+    @changelog
   end
 end
--- spec/githubify_spec.rb
+++ spec/githubify_spec.rb
@@ -9,4 +9,21 @@ describe Githubifier do
       g.changelog.should == 'ChangeLog'
     end
   end
+
+  describe "#better_changelog" do
+    let(:user) { 'gregbell' }
+    let(:project) { 'activeadmin' }
+
+    let(:better_changelog) { Githubifier.new(user, project, changelog).better_changelog }
+
+    subject { better_changelog }
+
+    context "when the changelog does not contain any reference to issues or users" do
+      let(:changelog) { 'ChangeLog' }
+
+      it "should return the original changelog" do
+        better_changelog.should == changelog
+      end
+    end
+  end
 end
