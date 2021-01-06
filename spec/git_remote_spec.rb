56db40de5cfe934d2fb00282e034979a46206908
Marco Costa <mmarcottulio@gmail.com>
2021-01-06 17:49:22 -0500

    Retrieve git-remote from local configuration (#17)

--- spec/git_remote_spec.rb
+++ spec/git_remote_spec.rb
@@ -14,4 +14,13 @@ describe GitRemote do
     its(:user) { should == 'pcreux' }
     its(:project) { should == 'pimpmychangelog' }
   end
+
+  context "when no URL is passed" do
+    subject { described_class.new }
+
+    # We can't match an exact user, as this depends on who
+    # has checked out this repository.
+    its(:user) { should be_a(String) }
+    its(:project) { should == 'pimpmychangelog' }
+  end
 end
